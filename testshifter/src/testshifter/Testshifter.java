0f405d99107bc9b3dcf1ecde8fb2a68f26957012
Jordan Conner <321j.con@gmail.com>
2014-04-22 15:35:17 -0400

    bit shifter

---
+++ testshifter/src/testshifter/Testshifter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testshifter;
+
+/**
+ *
+ * @author jconner
+ */
+public class Testshifter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1000;
+        System.out.println(0x02);
+        System.out.println(1 << 2);
+        System.out.println(i*Math.pow(2, 4));
+        System.out.println(0x01 & 0x02);
+        System.out.println(62 & 2);
+    }
+    
+}
